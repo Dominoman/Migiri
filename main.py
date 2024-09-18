84df7276c98d28b44b4273075b5947268afc5e71
Dominoman <olaca@freemail.hu>
2024-09-18 20:44:22 +0200

    Pageing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from sqlalchemy import select, insert
+from sqlalchemy import select, insert, func
 from tqdm import tqdm
 
 import config
 from database import Database, Base
+
+YIELD_COUNT=1000
 
 source=Database(config.DB_SOURCE,config.DB_DEBUG)
 c_source=source.engine.connect()
@@ -10,10 +12,13 @@ destination=Database(config.DB_DESTINATION,config.DB_DEBUG)
 c_destination=destination.engine.connect()
 
 for table in ['search','itinerary','route','itinerary2route','routehistory']:
-    result=c_source.execute(select(Base.metadata.tables[table])).all()
+    table_object=Base.metadata.tables[table]
+    count=c_source.execute(select(func.count()).select_from(table_object)).scalar()
+    result=c_source.execute(select(table_object)).yield_per(YIELD_COUNT)
+    print(result.count())
 
-    insert_stmt = insert(Base.metadata.tables[table])
-    for row in tqdm(result,desc=table):
+    insert_stmt = insert(table_object)
+    for row in tqdm(result,desc=table,total=count):
         c_destination.execute(insert_stmt,row._mapping)
     c_destination.commit()
 
